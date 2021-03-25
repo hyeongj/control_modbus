15146fa4580ee507074db273bf3736f5a0f30d68
Hyeong Jae Lee <hul143@gmail.com>
2021-03-25 13:49:57 -0700

    update

--- control.py
+++ control.py
@@ -3,6 +3,8 @@ import time
 
 
 iIP ='127.0.0.1' 
+iIP ='192.168.1.42' 
+
 def modconnect(IP):
 	PORT=1502
 	cli = ModbusClient(host=IP, port=PORT)
@@ -27,13 +29,28 @@ while x!=0:
 		MODBUS_GET=cli.read_holding_registers(40001,19)
 		time.sleep(0.5)
 		MODBUS_DATA=MODBUS_GET.registers
+		'''
+		_temp1 = box temp
+		_temp2 = TR1 temp
+		_temp3 = TR2 temp
+		'''
 		JSONDATA={'_speed':MODBUS_DATA[0], '_buffer':(MODBUS_DATA[2]-1)*100,'_time_in':MODBUS_DATA[1], '_maxth':MODBUS_DATA[3], '_gain':MODBUS_DATA[4],
 					 '_rate':MODBUS_DATA[5], '_sel_channel':MODBUS_DATA[8], '_sel_amp':MODBUS_DATA[9],  
 					'_height1':MODBUS_DATA[10]/10,  '_height2':MODBUS_DATA[11]/10,  '_height3':MODBUS_DATA[12]/10,  '_height4':MODBUS_DATA[13]/10,
 					'_humidity':MODBUS_DATA[15],  '_temp1':MODBUS_DATA[14],  '_temp2':MODBUS_DATA[16],  '_temp3':MODBUS_DATA[17],
 					 '_CNT_THRESHOLD':MODBUS_DATA[18]
 				}
-		print(JSONDATA)
+		tof = 1e6*2*JSONDATA["_height1"]*0.0254/JSONDATA["_speed"]
+
+		print(f'Buffer: {JSONDATA["_buffer"]}')
+		print(f'Threshold time: {JSONDATA["_time_in"]}')
+		print(f'RATE: {JSONDATA["_rate"]}')
+		print(f'TR1 TOF: {tof} us')
+		print(f'TR1 Height: {JSONDATA["_height1"]}  inch')
+		print(f'Humidity: {JSONDATA["_humidity"]} %')
+		print(f'Box Temp: {JSONDATA["_temp1"]}')
+		print(f'TR1 Temp: {JSONDATA["_temp2"]}')
+		print(f'TR2 Temp: {JSONDATA["_temp3"]}')
 
 
 '''
